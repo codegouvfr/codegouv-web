4072a7499af9a2db405811e7ddb4876d66577983
garronej <joseph.garrone@data.gouv.fr>
2023-03-14 11:24:58 +0100

    Demo component

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,9 +1,23 @@
 
+import { MyComponent } from "ui/shared/MyComponent";
+import { useStyles } from "@codegouvfr/react-dsfr/tss";
+import { fr } from "@codegouvfr/react-dsfr";
 
-export function Home(){
+export function Home() {
+
+	const { css } = useStyles();
 
 	return (
-		<h1>Hello world Home</h1>
+		<>
+			<h1>Hello world Home</h1>
+			<MyComponent
+				text="Salut ça va?"
+				className={css({
+					marginTop: fr.spacing("12v"),
+					marginLeft: fr.spacing("24v")
+				})}
+			/>
+		</>
 	);
 
 
