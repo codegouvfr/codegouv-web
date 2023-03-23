5a7c5c59889da0afbb2dc5298f8c0992abf8e0d4
Robin Minervini <robin.minervini@gmail.com>
2023-03-23 16:19:33 +0100

    add new TilesColumns and Contribute component and their stories

--- src/ui/shared/SiteStats.tsx
+++ src/ui/shared/SiteStats.tsx
@@ -1,11 +1,5 @@
-import { declareComponentKeys } from "i18nifty";
-import { useTranslation } from "ui/i18n";
 import { fr } from "@codegouvfr/react-dsfr";
 import { makeStyles } from "tss-react/dsfr";
-import Tile from "@codegouvfr/react-dsfr/Tile";
-import Card from "@codegouvfr/react-dsfr/Card";
-import { Button } from "@codegouvfr/react-dsfr/Button";
-import Input from "@codegouvfr/react-dsfr/Input";
 
 type Props = {
     title?: string
@@ -17,22 +11,21 @@ type Props = {
 }
 export const SiteStats = (props: Props) => {
     const { stats, title, invertedColors } = props
-    const { t: tCommons } = useTranslation({ App: null });
     const { cx, classes } = useStyles({ invertedColors, statsCount: stats.length });
 
     return (
-        <div className={cx(fr.cx("fr-container"), classes.numbersListContainer)}>
-            <h1 className={cx(classes.whiteText, classes.numberTitle)}>
+        <div className={cx(fr.cx("fr-container"), classes.statsListContainer)}>
+            <h1 className={cx(classes.whiteText, classes.statTitle)}>
                 { title }
             </h1>
-            <div className={classes.grid3items}>
+            <div className={classes.grid}>
                 {stats.map(stat => (
                     <div key={stat.label}>
                         <p
                             className={cx(
                                 fr.cx("fr-display--sm"),
                                 classes.whiteText,
-                                classes.numberText
+                                classes.statNumber
                             )}
                         >
                             {stat.number}
@@ -46,45 +39,10 @@ export const SiteStats = (props: Props) => {
 }
 
 const useStyles = makeStyles<{ invertedColors: Props["invertedColors"], statsCount: number }>()((theme, { invertedColors, statsCount }) => ({
-    section: {
-        ...fr.spacing("padding", {
-            topBottom: "30v"
-        }),
-        [fr.breakpoints.down("md")]: {
-            ...fr.spacing("padding", {
-                topBottom: "20v"
-            })
-        }
-    },
-    backgroundFullWidth: {
-        backgroundColor: theme.decisions.background.actionHigh.blueFrance.default
-    },
-    titleSection: {
-        marginBottom: fr.spacing("10v"),
-        [fr.breakpoints.down("md")]: {
-            marginBottom: fr.spacing("8v")
-        }
-    },
-    titleContainer: {
-        marginBottom: fr.spacing("10v"),
-        display: "flex"
-    },
-    intro: {
-        display: "flex",
-        gap: fr.spacing("6v"),
-        [fr.breakpoints.down("md")]: {
-            flexDirection: "column"
-        }
-    },
-    primaryEvents: {
-        display: "flex",
-        flexDirection: "column",
-        gap: fr.spacing("8v")
-    },
-    numbersListContainer: {
+    statsListContainer: {
         textAlign: "center"
     },
-    grid3items: {
+    grid: {
         display: "grid",
         gridTemplateColumns: `repeat(${statsCount}, 1fr)`,
         columnGap: fr.spacing("6v"),
@@ -96,27 +54,10 @@ const useStyles = makeStyles<{ invertedColors: Props["invertedColors"], statsCou
     whiteText: {
         color: invertedColors ? theme.decisions.text.inverted.grey.default : ""
     },
-    numberTitle: {
+    statTitle: {
         marginBottom: fr.spacing("20v")
     },
-    numberText: {
+    statNumber: {
         marginBottom: fr.spacing("1v")
     },
-    shareGroup: {
-        order: "initial",
-        marginBottom: fr.spacing("4v"),
-
-        ".fr-share__link": {
-            marginBottom: 0
-        }
-    },
-    row: {
-        "& .fr-col-4": {
-            [fr.breakpoints.down("md")]: {
-                flex: "0 0 100%",
-                width: "100%",
-                maxWidth: "100%"
-            },
-        }
-    }
 }));
---
+++ src/ui/shared/TileColumns.tsx
@@ -0,0 +1,40 @@
+import { assert } from "tsafe/assert";
+import type { Equals } from "tsafe";
+import { makeStyles } from "tss-react/dsfr";
+import Tile, { TileProps } from "@codegouvfr/react-dsfr/Tile";
+import { fr } from "@codegouvfr/react-dsfr";
+
+type Props = {
+    className?: string
+    title?: string
+    tileList: TileProps[]
+}
+
+export const TileColumns = (props: Props) => {
+
+    const {className, title, tileList, ...rest} = props
+    assert<Equals<typeof rest, {}>>()
+
+    const {classes} = useStyles({ tileCount: tileList.length });
+
+    return (
+        <>
+            {title && <h1>{ title }</h1>}
+            <div className={classes.grid}>
+                { tileList.map(tile => <Tile {...tile} /> ) }
+            </div>
+        </>
+    )
+}
+
+const useStyles = makeStyles<{ tileCount: number }>()((theme, { tileCount }) => ({
+    grid: {
+        display: "grid",
+        gridTemplateColumns: `repeat(${tileCount}, 1fr)`,
+        columnGap: fr.spacing("6v"),
+        [fr.breakpoints.down("md")]: {
+            gridTemplateColumns: `repeat(1, 1fr)`,
+            rowGap: fr.spacing("4v")
+        },
+    },
+}));
